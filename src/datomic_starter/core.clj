3352c46f466c9910af64acdc7efb2b24f350f99c
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-05-10 15:23:30 +0200

    Initial

---
+++ src/datomic_starter/core.clj
@@ -0,0 +1,7 @@
+(ns datomic-starter.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/datomic_starter/core_test.clj
@@ -0,0 +1,7 @@
+(ns datomic-starter.core-test
+  (:require [clojure.test :refer :all]
+            [datomic-starter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
